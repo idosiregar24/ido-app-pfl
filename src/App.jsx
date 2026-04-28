b428d516c1b124dd696bdf9d8b559b4c121fbf6c
Ido Refael Siregar <ido24si@mahasiswa.pcr.ac.id>
2026-04-28 15:36:07 +0700

    Login API + Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./assets/tailwind.css";
 
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Loading from "./components/Loading";
 // import { ERROR_CONFIG } from "./pages/ErrorPage";
 
 // import Orders from "./pages/Orders";
@@ -25,64 +26,65 @@ const Login = React.lazy(() => import("./pages/auth/Login"));
 const Register = React.lazy(() => import("./pages/auth/Register"));
 const Forgot = React.lazy(() => import("./pages/auth/Forgot"));
 
-
 function App() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/customers" element={<Customers />} />
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
 
-        {/* Error Routes */}
-        <Route
-          path="/error/400"
-          element={
-            <ErrorPage
-              kodeError="400"
-              deskripsiError="Bad Request! Ada yang salah dengan permintaanmu."
-              gambarError="https://illustrations.popsy.co/blue/crashed-error.svg"
-            />
-          }
-        />
-        <Route
-          path="/error/401"
-          element={
-            <ErrorPage
-              kodeError="401"
-              deskripsiError="Unauthorized! Kamu tidak punya izin untuk mengakses halaman ini."
-              gambarError="https://illustrations.popsy.co/blue/access-denied.svg"
-            />
-          }
-        />
-        <Route
-          path="/error/403"
-          element={
-            <ErrorPage
-              kodeError="403"
-              deskripsiError="Forbidden! Akses ke sumber daya ini dilarang."
-              gambarError="https://illustrations.popsy.co/blue/forbidden.svg"
-            />
-          }
-        />
-        <Route
-          path="/error/404"
-          element={
-            <ErrorPage
-              kodeError="404"
-              deskripsiError="Not Found! Halaman yang kamu cari tidak dapat ditemukan."
-              gambarError="https://illustrations.popsy.co/blue/page-not-found.svg"
-            />
-          }
-        />
-      </Route>
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot" element={<Forgot />} />
-      </Route>
-    </Routes>
+          {/* Error Routes */}
+          <Route
+            path="/error/400"
+            element={
+              <ErrorPage
+                kodeError="400"
+                deskripsiError="Bad Request! Ada yang salah dengan permintaanmu."
+                gambarError="https://illustrations.popsy.co/blue/crashed-error.svg"
+              />
+            }
+          />
+          <Route
+            path="/error/401"
+            element={
+              <ErrorPage
+                kodeError="401"
+                deskripsiError="Unauthorized! Kamu tidak punya izin untuk mengakses halaman ini."
+                gambarError="https://illustrations.popsy.co/blue/access-denied.svg"
+              />
+            }
+          />
+          <Route
+            path="/error/403"
+            element={
+              <ErrorPage
+                kodeError="403"
+                deskripsiError="Forbidden! Akses ke sumber daya ini dilarang."
+                gambarError="https://illustrations.popsy.co/blue/forbidden.svg"
+              />
+            }
+          />
+          <Route
+            path="/error/404"
+            element={
+              <ErrorPage
+                kodeError="404"
+                deskripsiError="Not Found! Halaman yang kamu cari tidak dapat ditemukan."
+                gambarError="https://illustrations.popsy.co/blue/page-not-found.svg"
+              />
+            }
+          />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
